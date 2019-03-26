8fa7c80c8155d73ed83c64c1621747b434d0465d
Jeff Rummel <jeff@jeffrummel.com>
2019-03-26 11:36:05 -0400

    wires up SCSS

--- gulp/styles.js
+++ gulp/styles.js
@@ -1,5 +1,6 @@
-var project = require('./_project.js');
-var gulp    = require('gulp');
+var project = require('./_project.js')
+var gulp    = require('gulp')
+var sass    = require('gulp-sass')
 var postcss = require('gulp-postcss');
 
 //  Plugins
@@ -7,16 +8,22 @@ var precss  = require('precss');
 var csso    = require('postcss-csso');
 var pixelstorem = require('postcss-pixels-to-rem');
 
-// Compile CSS
 gulp.task('styles', function() {
-  var processors = [
+  return gulp.src(project.src + '/css/*.scss')
+    .pipe(sass({
+      outputStyle: 'uncompressed'
+    }).on('error', sass.logError))
+    .pipe(gulp.dest(project.dest + '/css/'))
+})
+gulp.task('styles:production', function() {
+  var postProcessor = [
     pixelstorem({
       base: 16
     }),
     precss(),
     csso({forceMediaMerge: true})
   ];
-  return gulp.src(project.src + '/css/*.css')
-    .pipe(postcss(processors))
-    .pipe(gulp.dest(project.dest + '/css'));
-});
+  return gulp.src(project.dest + '/css/*.css')
+    .pipe(postcss(postProcessor))
+    .pipe(gulp.dest(project.dest + '/css'))
+})
